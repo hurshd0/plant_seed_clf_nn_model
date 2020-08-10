48552a8716633dcf0d55ced81a684c1fb8eb7687
Harsh <hrdesai007@gmail.com>
2020-08-10 17:50:02 +0000

    setup for cnn sagemaker training

---
+++ scripts/fetch_kaggle_dataset.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
+NOW=$(date)
+
+kaggle datasets download -d $TRAINING_DATA_URL -p packages/cnn_model/datasets/ && \
+unzip packages/cnn_model/datasets/v2-plant-seedlings-dataset.zip -d packages/cnn_model/datasets/v2-plant-seedlings-dataset && \
+echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/cnn_model/datasets/training_data_reference.txt && \
+mkdir -p "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
+mv -v "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse/"* "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
+rm -rf "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse"
