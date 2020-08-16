088cae2764b41aeb0a0e88d2cb4539ac22cdf719
Harsh Desai <hurshd0@gmail.com>
2020-08-16 22:09:07 +0000

    updated fetch script, gitignore, and analysis notebook

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -6,6 +6,6 @@ NOW=$(date)
 kaggle datasets download -d $TRAINING_DATA_URL -p packages/cnn_model/datasets/ && \
 unzip packages/cnn_model/datasets/v2-plant-seedlings-dataset.zip -d packages/cnn_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/cnn_model/datasets/training_data_reference.txt && \
-mkdir -p "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
-mv -v "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse/"* "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
-rm -rf "./packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse"
+mkdir -p "../packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
+mv -v "../packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse/"* "../packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
+rm -rf "../packages/cnn_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse"
